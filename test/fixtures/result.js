493febc5279c28386ac95f7715cb35f6b022bd9c
Thomas Watson <w@tson.dk>
2019-04-09 19:53:37 +0200

    test: add tests for fs-based functions

---
+++ test/fixtures/result.js
@@ -0,0 +1,96 @@
+'use strict'
+
+module.exports = {
+  entries: [
+    {
+      id: '11',
+      groups: 'devices',
+      path: '/kubepods/besteffort/pod74c13223-5a00-11e9-b385-42010a80018d/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      controllers: ['devices'],
+      containerId: '34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      podId: '74c13223-5a00-11e9-b385-42010a80018d'
+    },
+    {
+      id: '10',
+      groups: 'perf_event',
+      path: '/kubepods/besteffort/pod74c13223-5a00-11e9-b385-42010a80018d/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      controllers: ['perf_event'],
+      containerId: '34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      podId: '74c13223-5a00-11e9-b385-42010a80018d'
+    },
+    {
+      id: '9',
+      groups: 'memory',
+      path: '/kubepods/besteffort/pod74c13223-5a00-11e9-b385-42010a80018d/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      controllers: ['memory'],
+      containerId: '34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      podId: '74c13223-5a00-11e9-b385-42010a80018d'
+    },
+    {
+      id: '8',
+      groups: 'freezer',
+      path: '/kubepods/besteffort/pod74c13223-5a00-11e9-b385-42010a80018d/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      controllers: ['freezer'],
+      containerId: '34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      podId: '74c13223-5a00-11e9-b385-42010a80018d'
+    },
+    {
+      id: '7',
+      groups: 'hugetlb',
+      path: '/kubepods/besteffort/pod74c13223-5a00-11e9-b385-42010a80018d/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      controllers: ['hugetlb'],
+      containerId: '34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      podId: '74c13223-5a00-11e9-b385-42010a80018d'
+    },
+    {
+      id: '6',
+      groups: 'cpuset',
+      path: '/kubepods/besteffort/pod74c13223-5a00-11e9-b385-42010a80018d/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      controllers: ['cpuset'],
+      containerId: '34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      podId: '74c13223-5a00-11e9-b385-42010a80018d'
+    },
+    {
+      id: '5',
+      groups: 'blkio',
+      path: '/kubepods/besteffort/pod74c13223-5a00-11e9-b385-42010a80018d/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      controllers: ['blkio'],
+      containerId: '34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      podId: '74c13223-5a00-11e9-b385-42010a80018d'
+    },
+    {
+      id: '4',
+      groups: 'cpu,cpuacct',
+      path: '/kubepods/besteffort/pod74c13223-5a00-11e9-b385-42010a80018d/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      controllers: ['cpu', 'cpuacct'],
+      containerId: '34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      podId: '74c13223-5a00-11e9-b385-42010a80018d'
+    },
+    {
+      id: '3',
+      groups: 'net_cls,net_prio',
+      path: '/kubepods/besteffort/pod74c13223-5a00-11e9-b385-42010a80018d/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      controllers: ['net_cls', 'net_prio'],
+      containerId: '34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      podId: '74c13223-5a00-11e9-b385-42010a80018d'
+    },
+    {
+      id: '2',
+      groups: 'pids',
+      path: '/kubepods/besteffort/pod74c13223-5a00-11e9-b385-42010a80018d/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      controllers: ['pids'],
+      containerId: '34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      podId: '74c13223-5a00-11e9-b385-42010a80018d'
+    },
+    {
+      id: '1',
+      groups: 'name=systemd',
+      path: '/kubepods/besteffort/pod74c13223-5a00-11e9-b385-42010a80018d/34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      controllers: ['name=systemd'],
+      containerId: '34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+      podId: '74c13223-5a00-11e9-b385-42010a80018d'
+    }
+  ],
+  containerId: '34dc0b5e626f2c5c4c5170e34b10e7654ce36f0fcd532739f4445baabea03376',
+  podId: '74c13223-5a00-11e9-b385-42010a80018d'
+}
